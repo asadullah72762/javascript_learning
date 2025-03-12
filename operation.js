0bc46b5c101ee4c555ceddb53806e6635b9dcece
asadullah72762 <156236337+asadullah72762@users.noreply.github.com>
2025-03-12 18:20:09 +0000

    operations

---
+++ operation.js
@@ -0,0 +1,30 @@
+// //operation 
+
+let A=(5+3);
+console.log(A);
+
+let B=(5-3);
+console.log(B);
+
+let C=(5*3);
+console.log(C);
+
+let D=(5**3);
+console.log(D);
+
+let E=(5/3);
+console.log(E);
+
+let F=(5%3);
+console.log(F);
+
+// find negative value
+
+let value=8;
+let negvalue = -value;
+console.log(negvalue);
+
+
+
+
+
