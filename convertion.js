8cb4dbfad89bf62b5c8770ae7d7ea17661f603e4
asadullah72762 <156236337+asadullah72762@users.noreply.github.com>
2025-03-12 18:43:58 +0000

    conversion

---
+++ convertion.js
@@ -0,0 +1,47 @@
+// convertion string to number
+
+let score = "66asad";//output is NaN
+// let score="66";//Output is 66
+// let score=true;//output is 1
+// let score=null;//output is 0
+// let score=undefined;
+
+console.log(typeof score);
+console.log(typeof(score));//these two method to find types
+
+let valueinnumber =Number(score);
+console.log(valueinnumber);
+
+// let score="66";//Output is 66
+
+// console.log(typeof score);
+// console.log(typeof(score));//these two method to find types
+
+// let valueinnumber =Number(score);
+// console.log(valueinnumber);
+
+// let score=true;//output is 1
+
+// console.log(typeof score);
+// console.log(typeof(score));//these two method to find types
+
+// let valueinnumber =Number(score);
+// console.log(valueinnumber);
+
+
+// let score=null;//output is 0
+// console.log(typeof score);
+// console.log(typeof(score));//these two method to find types
+
+// let valueinnumber =Number(score);
+// console.log(valueinnumber);
+
+
+
+// let score=undefined;
+
+// console.log(typeof score);
+// console.log(typeof(score));//these two method to find types
+
+// let valueinnumber =Number(score);
+// console.log(valueinnumber);
