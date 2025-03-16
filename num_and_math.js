2ccbea0e374c4ca6a7b9d9b6dfe17eeee4afd2ee
asadullah72762 <156236337+asadullah72762@users.noreply.github.com>
2025-03-17 00:24:08 +0500

    Number and maths

---
+++ num_and_math.js
@@ -0,0 +1,43 @@
+const score = 400;
+console.log(score);
+
+const balance  = new Number(100);
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+
+console.log(balance.toFixed());
+
+const anothernuber = 12323.4567;
+console.log(anothernuber);
+
+console.log(anothernuber.toPrecision(2));
+console.log(anothernuber.toFixed());
+
+// console.log(anothernuber.toPrecision(5));
+
+// console.log(anothernuber.toPrecision(3));
+
+const hundred = 1000000;
+console.log(hundred.toLocaleString());
+
+console.log(hundred.toLocaleString('en-PK'));
+
+// ******************Math************************
+console.log(Math);
+console.log(Math.abs(-4));
+
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(5.6));
+// console.log(Math.floor(9.9));
+// console.log(Math.min(3,4,6,5));
+// console.log(Math.max(1,2,3,4));
+
+console.log(Math.random());
+
+
+
+
+
+
